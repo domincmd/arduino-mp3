0aa4f5944ed0a3a3fbd98eb29a7aafb86ad651cf
domincmd <81363461+domincmd@users.noreply.github.com>
2026-06-30 02:06:09 +0000

    modified arduino file for 2 buttons
    im lazy

--- mp3.ino
+++ mp3.ino
@@ -4,39 +4,38 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-#define LEFT_PIN 2
-#define MIDDLE_PIN 3
-#define RIGHT_PIN 4
+#define MIDDLE_PIN 6
+#define RIGHT_PIN 8
 
 // Initialize software serial on pins 10 and 11
-SoftwareSerial mySoftwareSerial(10, 11);  // RX, TX
+SoftwareSerial mySoftwareSerial(12, 10);  // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
 
 int folder = 1;
 int song = 1;
 
-
-//each unit here corresponds to 100ms
+// each unit here corresponds to 100ms
 int doubleclickBuffer = 0;
+int rightholdBuffer = 0;
 
 bool leftInput = false;
 bool middleInput = false;
 bool rightInput = false;
 
 bool playing = false;
-bool songMode = true; //if true, selecting songs. if false, selecting folders
+bool songMode = true; // if true, selecting songs. if false, selecting folders
 
 void setup() {
   //setting up pins
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  pinMode(MIDDLE_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
 
   // Serial communication with the module
   mySoftwareSerial.begin(9600);
+
   // Initialize Arduino serial
   Serial.begin(115200);
-  // Check if the module is responding and if the SD card is found
+
   Serial.println();
   Serial.println(F("DFRobot DFPlayer Mini"));
   Serial.println(F("Initializing DFPlayer module ... Wait!"));
@@ -45,20 +44,17 @@ void setup() {
     Serial.println(F("Not initialized:"));
     Serial.println(F("1. Check the DFPlayer Mini connections"));
     Serial.println(F("2. Insert an SD card"));
-    while (true)
-      ;
+    while (true) {
+      delay(100);
+    }
   }
+
   Serial.println();
   Serial.println(F("DFPlayer Mini module initialized!"));
-  // Initial settings
-  myDFPlayer.setTimeOut(500);  // Serial timeout 500ms
-  myDFPlayer.volume(15);        // Volume 5
-  myDFPlayer.EQ(0);            // Normal equalization
-  //  myDFPlayer.EQ(DFPLAYER_EQ_POP);
-  //  myDFPlayer.EQ(DFPLAYER_EQ_ROCK);
-  //  myDFPlayer.EQ(DFPLAYER_EQ_JAZZ);
-  //  myDFPlayer.EQ(DFPLAYER_EQ_CLASSIC);
-  //  myDFPlayer.EQ(DFPLAYER_EQ_BASS);
+
+  myDFPlayer.setTimeOut(500);
+  myDFPlayer.volume(15);
+  myDFPlayer.EQ(0);
 
   changeMusic();
 }
@@ -69,61 +65,92 @@ void changeMusic() {
 }
 
 void loop() {
-  leftInput = !digitalRead(LEFT_PIN);
+
   middleInput = !digitalRead(MIDDLE_PIN);
   rightInput = !digitalRead(RIGHT_PIN);
 
+  // ---------------- MIDDLE BUTTON ----------------
+
   if (middleInput) {
-    if (doubleclickBuffer > 0) { //this only repeats once
+
+    if (doubleclickBuffer > 0) {
       Serial.println("FUCKIN CHANGE MODE");
       songMode = !songMode;
       doubleclickBuffer = 0;
+
+      while (!digitalRead(MIDDLE_PIN)) {
+        delay(50);
+      }
+
       return;
     }
+
     if (playing) {
       playing = false;
       myDFPlayer.pause();
-    }else{
+    } else {
       playing = true;
       myDFPlayer.start();
     }
-    while (!digitalRead(MIDDLE_PIN)) { //this needs constant updating
-      delay(50); //wait
+
+    while (!digitalRead(MIDDLE_PIN)) {
+      delay(50);
     }
+
     doubleclickBuffer = 7;
   }
 
-  if (leftInput) {
-    if (songMode) {
-      song = max(song-1, 1);
-    }else{
-      folder = max(folder-1, 1);
-      song = 1;
-    }
-    changeMusic();
-
-    while (!digitalRead(LEFT_PIN)) { //constant updating
-      delay(50);
-    }
-  }
+  // ---------------- RIGHT BUTTON ----------------
 
   if (rightInput) {
-    if (songMode) {
-      song = min(song+1, 99);
-    }else{
-      folder = min(folder+1, 99);
-      song = 1;
-    }
-    changeMusic();
 
-    while (!digitalRead(RIGHT_PIN)) { //constant updating
-      delay(50);
+    bool holdTriggered = false;
+    rightholdBuffer = 0;
+
+    while (!digitalRead(RIGHT_PIN)) {
+
+      delay(100);
+      rightholdBuffer++;
+
+      if (!holdTriggered && rightholdBuffer >= 10) {
+
+        holdTriggered = true;
+
+        Serial.println("PREVIOUS MUSIC");
+
+        if (songMode) {
+          song = max(song - 1, 1);
+        } else {
+          folder = max(folder - 1, 1);
+          song = 1;
+        }
+
+        changeMusic();
+      }
+    }
+
+    if (!holdTriggered) {
+
+      Serial.println("NEXT MUSIC");
+
+      if (songMode) {
+        song = min(song + 1, 99);
+      } else {
+        folder = min(folder + 1, 99);
+        song = 1;
+      }
+
+      changeMusic();
     }
   }
 
+  // ---------------- BUFFERS ----------------
 
-  doubleclickBuffer = max(doubleclickBuffer-1, 0);
-  Serial.println(doubleclickBuffer);
-  
+  doubleclickBuffer = max(doubleclickBuffer - 1, 0);
+
+  Serial.print(digitalRead(MIDDLE_PIN));
+  Serial.print(digitalRead(RIGHT_PIN));
+  Serial.println(rightholdBuffer);
+
   delay(100);
 }
